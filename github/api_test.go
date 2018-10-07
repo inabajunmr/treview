fac35bc6e2aaa983df47a1581f1770cfee39334c
inaba jun <inabajun.for.regi@gmail.com>
2018-10-07 11:53:21 +0900

    Add test (#17)
    * add test

--- github/api_test.go
+++ github/api_test.go
@@ -27,3 +27,45 @@ func TestFindSpecificLang(t *testing.T) {
 		t.Fatal("Not return 25 repositories.", err)
 	}
 }
+
+func TestPrint(t *testing.T) {
+	// This only and Pring
+	r := Repository{Name: "testName", URL: "http://example.com/test",
+		Description: "desc", Lang: "go", Star: 30, StarBySpan: 10, Fork: 10}
+
+	r.Print()
+}
+
+func TestGetQueryForSpan(t *testing.T) {
+	if value := getQueryForSpan(Today); value != "today" {
+		t.Fatal("It's not expected value by Today. value:" + value)
+	}
+
+	if value := getQueryForSpan(Week); value != "week" {
+		t.Fatal("It's not expected value by Week. value:" + value)
+	}
+
+	if value := getQueryForSpan(Month); value != "month" {
+		t.Fatal("It's not expected value by Month. value:" + value)
+	}
+
+}
+
+func TestGetSpanByString(t *testing.T) {
+	if value := GetSpanByString("today"); value != Today {
+		t.Fatal("It's not expected value by today. value:" + getQueryForSpan(value))
+	}
+
+	if value := GetSpanByString("week"); value != Week {
+		t.Fatal("It's not expected value by week. value:" + getQueryForSpan(value))
+	}
+
+	if value := GetSpanByString("month"); value != Month {
+		t.Fatal("It's not expected value by month. value:" + getQueryForSpan(value))
+	}
+
+	if value := GetSpanByString("other"); value != Today {
+		t.Fatal("It's not expected value by today. value:" + getQueryForSpan(value))
+	}
+
+}
