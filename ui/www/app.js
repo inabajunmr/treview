e34683f4170f8252ecc6646763a25b24bf23bd15
inaba jun <inabajun.for.regi@gmail.com>
2020-02-15 17:51:18 +0900

    fix Lgtm alert (#67)
    * lgtm
    
    * update

--- ui/www/app.js
+++ ui/www/app.js
@@ -1,37 +1,43 @@
 let vm = new Vue({
-    el: '#app',
-    components: { VoerroTagsInput },
-    data: {
-        repos: [],
-        langs: [],
-        condition: {
-            Langs: [],
-            CloneLangs: [],
-            Span: "today",
-            OnlyNew: true
-        },
-    },
-    watch: {
-        condition: {
-            handler: function (val, oldVal) {
-                reload(val)
-                if(val.Langs.slice().sort().toString() != val.CloneLangs.slice().sort().toString()) {
-                    console.log("update config")
-                    updateConfig(val.Langs)
-                    val.CloneLangs = val.Langs.slice()
-                }
-            },
-            deep: true
-        }
-    },
-    computed: {
-        langObjects: function () {
-            var obj = {};
-            for (val of this.langs) {
-                obj[val] = val;
-            }
-            return obj
-        }
+  el: "#app",
+  components: { VoerroTagsInput },
+  data: {
+    repos: [],
+    langs: [],
+    condition: {
+      Langs: [],
+      CloneLangs: [],
+      Span: "today",
+      OnlyNew: true
     }
-
-})
+  },
+  watch: {
+    condition: {
+      handler: function(val, oldVal) {
+        reload(val);
+        if (
+          val.Langs.slice()
+            .sort()
+            .toString() !=
+          val.CloneLangs.slice()
+            .sort()
+            .toString()
+        ) {
+          console.log("update config");
+          updateConfig(val.Langs);
+          val.CloneLangs = val.Langs.slice();
+        }
+      },
+      deep: true
+    }
+  },
+  computed: {
+    langObjects: function() {
+      let obj = {};
+      for (let val of this.langs) {
+        obj[val] = val;
+      }
+      return obj;
+    }
+  }
+});
